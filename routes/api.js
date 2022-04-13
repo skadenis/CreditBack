9ba352396bcb7e85f7f2a7a4e7d88d872b74b5c3
skadenis@mail.ru <Tutanu2211>
2022-04-13 17:41:35 +0300

    qwe

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,13 @@ let {GetSourceID} = require('../components/helpers')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let data = await new DataBase('leads').getAll();
-    res.json(data);
+    let yesterday = await new DataBase().DB_query('    SELECT source.name, count(*) FROM leads JOIN source on source.id = leads.source where to_char( now() - INTERVAL \'1 DAY\', \'YYYY-MM-DD\') = to_char(time, \'YYYY-MM-DD\') GROUP BY source.name;\n')
+    let today = await new DataBase().DB_query('    SELECT source.name, count(*) FROM leads JOIN source on source.id = leads.source where to_char( now() - INTERVAL \'0 DAY\', \'YYYY-MM-DD\') = to_char(time, \'YYYY-MM-DD\') GROUP BY source.name;\n')
+
+    res.json({
+        yesterday: yesterday,
+        today: today
+    })
 });
 
 router.post('/add-lead', Policy(), async function(req, res, next) {
